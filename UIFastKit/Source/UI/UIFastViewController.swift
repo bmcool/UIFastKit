338b45325875d0568ee777a072dabec3a8de6f42
bmcool <bloggerbm@gmail.com>
2020-11-20 09:47:18 +0800

    rename root scrollView

--- UIFastKit/Source/UI/UIFastViewController.swift
+++ UIFastKit/Source/UI/UIFastViewController.swift
@@ -13,7 +13,7 @@ public class RootFlexContainer: UIView {
 }
 
 open class UIFastViewController: UIViewController {
-    public let scrollView = UIScrollView()
+    public let rootScrollView = UIScrollView()
     
     public let rootFlexContainer = RootFlexContainer()
     public var rootLayoutMode = Flex.LayoutMode.adjustHeight
@@ -23,21 +23,21 @@ open class UIFastViewController: UIViewController {
         
         rootFlexContainer.viewController = self
         
-        view.addSubview(scrollView)
-        scrollView.addSubview(rootFlexContainer)
+        view.addSubview(rootScrollView)
+        rootScrollView.addSubview(rootFlexContainer)
     }
     
     open override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        scrollView.pin.all(view.pin.safeArea)
+        rootScrollView.pin.all(view.pin.safeArea)
         switch rootLayoutMode {
         case .adjustHeight: rootFlexContainer.pin.left().top().right()
         case .adjustWidth: rootFlexContainer.pin.left().top().bottom()
         case .fitContainer: rootFlexContainer.pin.all()
         }
         rootFlexContainer.box.layout(mode: rootLayoutMode)
-        scrollView.contentSize = rootFlexContainer.frame.size
+        rootScrollView.contentSize = rootFlexContainer.frame.size
     }
 }
 
