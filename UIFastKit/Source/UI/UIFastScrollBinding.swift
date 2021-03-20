25e41a593089bce4ab32fa4e93c37acb7d566ee5
bmcool <bloggerbm@gmail.com>
2021-03-20 17:18:14 +0800

    更靈活的處理Scroll Binder

--- UIFastKit/Source/UI/UIFastScrollBinding.swift
+++ UIFastKit/Source/UI/UIFastScrollBinding.swift
@@ -5,19 +5,11 @@
 //  Created by 林季正 on 2021/3/16.
 //
 
-public protocol UIFastScrollBindingContainerDataSource: class {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
-}
+import UIKit
 
-open class UIFastScrollBindingContainer: UIView {
-    public weak var dataSource: UIFastScrollBindingContainerDataSource? {
-        didSet {
-            scrollBindingTableViews.forEach { (view) in
-                view.dataSource = dataSource
-            }
-        }
-    }
-
+open class UIFastTableViewScrollBinder {
+    public init() { }
+    
     public var scrollBindingTableViews: [UIFastScrollBindingTableView] = [] {
         didSet {
             scrollBindingTableViews.forEach {[weak self] (view) in
@@ -26,69 +18,109 @@ open class UIFastScrollBindingContainer: UIView {
                 view.tableView.estimatedSectionHeaderHeight = 0
                 view.tableView.estimatedSectionFooterHeight = 0
                 
-                view.container = self
-                
-                view.dataSource = dataSource
+                view.binder = self
             }
         }
     }
     
-    func syncScroll(_ tableView: UITableView) {
+    func syncScroll(_ scrollView: UIScrollView) {
         scrollBindingTableViews.forEach { (tvc) in
-            if tvc.tableView != tableView {
-                tvc.tableView.contentOffset = tableView.contentOffset
+            if tvc.tableView != scrollView {
+                tvc.tableView.contentOffset = scrollView.contentOffset
             }
         }
     }
     
-    func tableViewDidScroll(_ tableView: UITableView) {
-        syncScroll(tableView)
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        syncScroll(scrollView)
     }
 }
 
-open class UIFastScrollBindingTableView: UIView {
-    public let tableView = UITableView()
-    
+open class UIFastScrollBindingTableViewProxy: NSObject {
     var isScrolling = false
     
-    public weak var container: UIFastScrollBindingContainer?
+    public weak var binder: UIFastTableViewScrollBinder?
     
-    public weak var dataSource: UIFastScrollBindingContainerDataSource?
+    public weak var delegate: UITableViewDelegate?
     
-    open func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return dataSource?.tableView(tableView, heightForRowAt: indexPath) ?? 0
+    open override func responds(to aSelector: Selector) -> Bool {
+        return super.responds(to: aSelector) || delegate?.responds(to: aSelector) == true
     }
-    
-    override public init(frame: CGRect) {
-        super.init(frame: .zero)
-        
-        tableView.delegate = self
-    }
-    
-    required public init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+
+    open override func forwardingTarget(for aSelector: Selector!) -> Any? {
+        if delegate?.responds(to: aSelector) == true {
+            return delegate
+        } else {
+            return super.forwardingTarget(for: aSelector)
+        }
     }
 }
 
-extension UIFastScrollBindingTableView: UITableViewDelegate {
+extension UIFastScrollBindingTableViewProxy: UITableViewDelegate {
     open func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         isScrolling = false
+        delegate?.scrollViewDidEndDecelerating?(scrollView)
     }
     
     open func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         if !decelerate {
             isScrolling = false
         }
+        delegate?.scrollViewDidEndDragging?(scrollView, willDecelerate: decelerate)
     }
     
     open func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         isScrolling = true
+        delegate?.scrollViewWillBeginDragging?(scrollView)
     }
     
     open func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if isScrolling {
-            container?.tableViewDidScroll(tableView)
+            binder?.scrollViewDidScroll(scrollView)
         }
+        delegate?.scrollViewDidScroll?(scrollView)
     }
 }
 
+open class UIFastScrollBindingTableView: UIView {
+    public let tableView = UITableView()
+    
+    private let defaultDelegate =  UIFastScrollBindingTableViewProxy()
+    
+    public weak var binder: UIFastTableViewScrollBinder? {
+        didSet {
+            defaultDelegate.binder = binder
+        }
+    }
+    
+    public weak var dataSource: UITableViewDataSource? {
+        didSet {
+            tableView.dataSource = dataSource
+        }
+    }
+    
+    public weak var delegate: UITableViewDelegate? {
+        didSet {
+            defaultDelegate.delegate = delegate
+            tableView.delegate = defaultDelegate
+        }
+    }
+    
+    override public init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        tableView.delegate = defaultDelegate
+        
+        addSubview(tableView)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        tableView.pin.all()
+    }
+}
